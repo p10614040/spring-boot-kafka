a1f481e98119bc7fe22f4bbe95369af21df96cf4
严亮 <695795023@qq.com>
2017-06-19 16:32:35 +0800

    增加 Kafka 生成者和消费者

---
+++ src/main/java/com/iceblcok/springboot/kafka/consumer/KafkaReceiverListener.java
@@ -0,0 +1,23 @@
+package com.iceblcok.springboot.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ *  Kafka 消息消费监听
+ *
+ * @author yan.liang@ustcinfo.com
+ * @date 2017/6/9
+ */
+@Component
+public class KafkaReceiverListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaReceiverListener.class);
+
+    @KafkaListener(topics = "iov-topic")
+    public void processMessage(String message) {
+        LOGGER.info("receive topic='{}' message='{}'", "iov-topic", message);
+    }
+}
